b116a3683a0a2f5aa55e6f2b23338e1a35768dad
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-05-06 18:04:04 +0300

    Start project

---
+++ src/main/java/kz/zhelezyaka/Spring6WebClientApplication.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6WebClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6WebClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/zhelezyaka/Spring6WebClientApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
