4ddc113b48eb3881c37ec02e2f8b3f6e8cb13037
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2023-05-19 21:49:04 +0300

    WebClient Filter Configuration

--- src/test/java/kz/zhelezyaka/client/BeerClientImplTest.java
+++ src/test/java/kz/zhelezyaka/client/BeerClientImplTest.java
@@ -10,7 +10,7 @@ import java.math.BigDecimal;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 import static org.awaitility.Awaitility.await;
-import static org.junit.jupiter.api.Assertions.*;
+
 
 @SpringBootTest
 class BeerClientImplTest {
